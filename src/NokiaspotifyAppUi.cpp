40c4daa10ceb906443051361225dc9861b28db3e
Bartosz Grzanka <1206459@pw.edu.pl>
2026-03-16 23:44:04 +0200

    rss definitions

--- src/NokiaspotifyAppUi.cpp
+++ src/NokiaspotifyAppUi.cpp
@@ -111,3 +111,113 @@ static HBufC8* ToAscii8LC(const TDesC& aText)
 	{
 	HBufC8* out = HBufC8::NewLC(aText.Length() + 1);
 	TPtr8 p = out->Des();
+	for (TInt i = 0; i < aText.Length(); ++i)
+		{
+		TUint c = aText[i];
+		if (c > 0x7F)
+			{
+			c = '?';
+			}
+		p.Append((TUint8)c);
+		}
+	return out;
+	}
+
+static void AppendLimited(TDes& aDst, const TDesC& aText)
+	{
+	const TInt free = aDst.MaxLength() - aDst.Length();
+	if (free <= 0)
+		{
+		return;
+		}
+	if (aText.Length() <= free)
+		{
+		aDst.Append(aText);
+		}
+	else
+		{
+		aDst.Append(aText.Left(free));
+		}
+	}
+
+static void AppendLineLimited(TDes& aDst, const TDesC& aText)
+	{
+	AppendLimited(aDst, aText);
+	AppendLimited(aDst, _L("\n"));
+	}
+
+static void TrimAsciiLine(TPtrC& aLine)
+	{
+	while (aLine.Length() > 0 && (aLine[0] == ' ' || aLine[0] == '\t' || aLine[0] == '\r'))
+		{
+		aLine.Set(aLine.Mid(1));
+		}
+	while (aLine.Length() > 0)
+		{
+		const TUint c = aLine[aLine.Length() - 1];
+		if (c == ' ' || c == '\t' || c == '\r')
+			{
+			aLine.Set(aLine.Left(aLine.Length() - 1));
+			}
+		else
+			{
+			break;
+			}
+		}
+	}
+
+static void AppendOwnedBufL(RPointerArray<HBufC>& aOut, const TDesC& aText);
+static void CopyAscii8ToDes(TDes& aOut, const TDesC8& aIn);
+
+static TBool IsLikelyTrackLine(const TDesC& aLine)
+	{
+	if (aLine.Length() < 2)
+		{
+		return EFalse;
+		}
+	if (aLine[0] == '<')
+		{
+		return EFalse;
+		}
+	if (aLine.FindF(_L("[ GRAJ ]")) >= 0 || aLine.FindF(_L("[ POBIERZ ]")) >= 0)
+		{
+		return EFalse;
+		}
+	if (aLine.FindF(_L("TURBOCONNECT")) >= 0 || aLine.FindF(_L("POBIERZ NOWY")) >= 0 || aLine.FindF(_L("FFmpeg")) >= 0)
+		{
+		return EFalse;
+		}
+	return ETrue;
+	}
+
+static TBool IsLikelyTrackLine8(const TDesC8& aLine)
+	{
+	if (aLine.Length() < 2)
+		{
+		return EFalse;
+		}
+	if (aLine[0] == '<')
+		{
+		return EFalse;
+		}
+	if (aLine.FindF(_L8("[ GRAJ ]")) >= 0 || aLine.FindF(_L8("[ POBIERZ ]")) >= 0)
+		{
+		return EFalse;
+		}
+	if (aLine.FindF(_L8("TURBOCONNECT")) >= 0 || aLine.FindF(_L8("POBIERZ NOWY")) >= 0 || aLine.FindF(_L8("FFmpeg")) >= 0)
+		{
+		return EFalse;
+		}
+	return ETrue;
+	}
+
+static void AppendOwnedBufFrom8L(RPointerArray<HBufC>& aOut, const TDesC8& aText8)
+	{
+	TBuf<256> temp;
+	const TInt n = (aText8.Length() < temp.MaxLength()) ? aText8.Length() : temp.MaxLength();
+	for (TInt i = 0; i < n; ++i)
+		{
+		temp.Append((TText)aText8[i]);
+		}
+	AppendOwnedBufL(aOut, temp);
+	}
